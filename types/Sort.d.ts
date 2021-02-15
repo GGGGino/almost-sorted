894b159ef9a415958c2376f642c75d3dcb9c8073
David Ginanni <david.ginanni@gmail.com>
2021-02-16 00:00:13 +0100

    added Some useful Types

--- types/Sort.d.ts
+++ types/Sort.d.ts
@@ -1,5 +1,5 @@
-export type ComparisionResult = 0 | 1 | -1;
+export type ComparisionResult = -1 | 0 | 1;
 
-export type Comparision = (first: any, second: any) => ComparisionResult;
+export type Compare<T> = (first: T, second: T) => ComparisionResult;
 
 export type KeyRetrivier = (element: any) => any;
