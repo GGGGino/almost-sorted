102df5a086e7b1df5026f0eec40b06876e4728bb
David Ginanni <david.ginanni@gmail.com>
2021-12-27 02:04:05 +0100

    Added some doc in selection sort

--- README.md
+++ README.md
@@ -1,2 +1,14 @@
 # almost-sorted
 Main sorting functions plus some extras
+
+[![Build Status](https://app.travis-ci.com/GGGGino/almost-sorted.svg?branch=main)](https://app.travis-ci.com/GGGGino/almost-sorted)
+
+## Sort
+
+### Selection sort
+
+Link to [section](src/methods/selectionSort.ts)
+
+## Credits
+
+Ascii graphics built thanks to [Ascii flow](https://asciiflow.com/)
--- src/methods/selectionSort.ts
+++ src/methods/selectionSort.ts
@@ -1,6 +1,28 @@
 import {Compare} from "../../types/Sort";
 import {swap} from "../utils";
 
+/*
+ * Loop all items to find the lowest/greatest
+ * and put it in first position.
+ *
+ *   0   1   2   3
+ * ┌───┬───┬───┬───┐
+ * │20 │12 │10 │15 │ minI=1
+ * └─┬─┴─▲─┴───┴───┘
+ *   └───┘
+ * ┌───┬───┬───┬───┐
+ * │20 │12 │10 │15 │ minI=2
+ * └───┴─┬─┴─▲─┴───┘
+ *       └───┘
+ * ┌───┬───┬───┬───┐
+ * │20 │12 │10 │15 │ minI=2
+ * └───┴───┴─┬─┴─▲─┘
+ *           └───┘
+ * ┌───┬───┬───┬───┐
+ * │20 │12 │10 │15 │ swapI:2->0
+ * └─▲─┴───┴─┬─┴───┘
+ *   └───────┘
+ */
 export default function<T>(array: T[], callback: Compare<T>) {
   for (let i = 0; i < array.length; i++) {
     let minorI = i;
